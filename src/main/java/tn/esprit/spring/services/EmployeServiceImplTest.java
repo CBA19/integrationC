543e489bfa4df2ad0c07177a10572f3e42304c7f
chamsBA <chamesseddine.benalaya@esprit.tn>
2020-10-30 23:40:54 +0100

    second commit

--- src/main/java/tn/esprit/spring/services/EmployeServiceImplTest.java
+++ src/main/java/tn/esprit/spring/services/EmployeServiceImplTest.java
@@ -2,6 +2,7 @@ package tn.esprit.spring.services;
 
 import static org.junit.Assert.assertEquals;
 
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,15 +12,12 @@ import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Role;
 import tn.esprit.spring.repository.EmployeRepository;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 public class EmployeServiceImplTest {
 	
 	@Autowired
