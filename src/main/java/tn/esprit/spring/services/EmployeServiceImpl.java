2ac3e93b6c013254221db3f02aabe4190945cdbf
chamsBA <chamesseddine.benalaya@esprit.tn>
2020-10-31 17:52:55 +0100

    timesheetModifJunit

--- src/main/java/tn/esprit/spring/services/EmployeServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EmployeServiceImpl.java
@@ -10,7 +10,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
+import  org.springframework.transaction.annotation.Transactional;
 
 import tn.esprit.spring.entities.Contrat;
 import tn.esprit.spring.entities.Departement;
--- src/main/java/tn/esprit/spring/services/EmployeServiceImplTest.java
+++ src/main/java/tn/esprit/spring/services/EmployeServiceImplTest.java
@@ -12,12 +12,15 @@ import org.apache.logging.log4j.Logger;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import tn.esprit.spring.entities.Employe;
 import tn.esprit.spring.entities.Role;
 import tn.esprit.spring.repository.EmployeRepository;
 
-
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class EmployeServiceImplTest {
 	
 	@Autowired
@@ -28,8 +31,8 @@ public class EmployeServiceImplTest {
 	@Test
 	public void testAuthenticate() throws ParseException {
 		l.info("In  authenticate : "); 
-		SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-		Date d = dateFormat.parse("2020-10-19");
+		//SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+		//Date d = dateFormat.parse("2020-10-31");
 		Employe emp=new Employe("benalaya", "chams", "chamesseddine.benalaya@esprit.tn",  "20205203",false, Role.ADMINISTRATEUR);
 		Employe e=employeServiceImpl.authenticate(emp.getEmail(), emp.getPassword());
 		assertEquals(emp.getPrenom(), e.getPrenom());
